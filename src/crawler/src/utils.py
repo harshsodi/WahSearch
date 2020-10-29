8ca1d44c4c6860fb220d382c4049d2c5b4063cd1
Harsh <harshsodi@gmail.com>
2020-10-28 22:02:45 -0400

    Re-structured components in separate directories

---
+++ src/crawler/src/utils.py
@@ -0,0 +1,38 @@
+import nltk
+import os
+
+class Utils:
+
+    def __init__(self):
+        self.load_stop_words()
+
+    def load_stop_words(self):
+        try:
+            dir_path = self.get_project_root()
+            file_path = os.path.join(dir_path, 'src', 'stop_words.dat')
+            stop_words_file = open(file_path)
+            stop_word_data = stop_words_file.read()
+            stop_words_list = stop_word_data.split('\n')
+
+            self.stop_words = dict.fromkeys(stop_words_list, True)
+            self.stemmer = nltk.SnowballStemmer("english")
+        
+        except Exception:
+            print ("Error while loading processing engine. WIll index useless stuff along with useful ones.")
+
+    def get_processed_words_list(self, words):
+
+        """
+        Process words list and return a searchable words list
+        remove stop words
+        stem remaining words
+        """
+
+        processed_list = []
+
+        for word in words:
+            if not self.stop_words.get(word):
+                word = self.stemmer.stem(word)
+                processed_list.append(word)
+
+        return processed_list
